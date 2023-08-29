eccfcad75cf37ef36d00974114b631fc0877e6b4
monu_jangid76 <jangidrohit817@gmail.com>
2023-08-29 18:00:59 +0530

    add components footer, navbar and category panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { CategoryPanelComponent } from './layout/navbar/category-panel/category-panel.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryPanelComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/navbar/category-panel/category-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-panel',
+  templateUrl: './category-panel.component.html',
+  styleUrls: ['./category-panel.component.scss']
+})
+export class CategoryPanelComponent {
+
+}
