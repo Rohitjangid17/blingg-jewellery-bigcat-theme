0d6b36c9f2acdcaeeaca3946e1a92515d53e3e5e
monu_jangid76 <jangidrohit817@gmail.com>
2023-08-30 13:35:31 +0530

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { CategoryPanelComponent } from './layout/navbar/category-panel/category-panel.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { CustomerStoreComponent } from './modules/store-customer/pages/customer-store/customer-store.component';
+import { AboutComponent } from './modules/store-customer/pages/about/about.component';
+import { ContactComponent } from './modules/store-customer/pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CategoryPanelComponent,
-    FooterComponent
+    FooterComponent,
+    CustomerStoreComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/store-customer/pages/customer-store/customer-store.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-store',
+  templateUrl: './customer-store.component.html',
+  styleUrls: ['./customer-store.component.scss']
+})
+export class CustomerStoreComponent {
+
+}
