27ed4d1eee8bbc901a629a2935311f3023fa26fc
monu_jangid76 <jangidrohit817@gmail.com>
2023-09-02 14:14:40 +0530

    add category panel

--- src/app/layout/navbar/category-panel/category-panel.component.ts
+++ src/app/layout/navbar/category-panel/category-panel.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-panel.component.scss']
 })
 export class CategoryPanelComponent {
+  allCategory: { id: number, categoryName: string }[] = [];
 
+  ngOnInit(): void {
+    this.allCategory = [
+      {
+        id: 1,
+        categoryName: "rings"
+      },
+      {
+        id: 2,
+        categoryName: "bracelets"
+      },
+      {
+        id: 3,
+        categoryName: "earrings"
+      },
+      {
+        id: 4,
+        categoryName: "necklaces"
+      },
+    ]
+  }
 }
