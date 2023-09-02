94577c13201fb02790b67ea9e9104cf0d0523886
monu_jangid76 <jangidrohit817@gmail.com>
2023-09-02 09:47:09 +0530

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerStoreComponent } from './modules/store-customer/pages/customer-store/customer-store.component';
+import { AboutComponent } from './modules/store-customer/pages/about/about.component';
+import { ContactComponent } from './modules/store-customer/pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: CustomerStoreComponent
+  },
+  {
+    path: "about", component: AboutComponent
+  },
+  {
+    path: "contact", component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
