2e13734e59d5b833bfdd3ff3b8355d83a278d946
monu_jangid76 <jangidrohit817@gmail.com>
2023-09-02 11:01:47 +0530

    navbar toggle change icon

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  openMenu: boolean = false;
 
+  onMenuOpen(openMenu: boolean) {
+    this.openMenu = openMenu;
+    return openMenu;
+  }
+
+  onMenuClose(closeMenu: boolean) {
+    this.openMenu = closeMenu;
+    return closeMenu;
+  }
 }
